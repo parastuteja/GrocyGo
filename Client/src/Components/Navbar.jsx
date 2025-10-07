770737bcea51d449f888ec80675d10040e763830
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-10-07 14:10:09 +0530

    Update Nav.js

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -33,9 +33,18 @@ function Navbar() {
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+               {!user ?( <button onClick={()=>{setShowUserLogin(true)}} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
-                </button>
+                </button>):(
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} alt="" className='w-10' />
+                        <ul className='hidden gorup-hover:block absolute top-10 right-0 bg-white shadow border
+                        border-grey-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                            <li onClick={()=>{navigate('my-orders')}} className='p-1.5 p1-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={logOut} className='p-1.5 p1-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                        </ul>
+                    </div>
+                )}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
--- Client/src/Context/AppContext.jsx
+++ Client/src/Context/AppContext.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 export const AppContext=createContext()
 export const AppContextProvider=({children})=>{
     const navigate=useNavigate()
-    const[user,setUser]=useState(null)
+    const[user,setUser]=useState(true)
     const [isSeller,setisSeller]=useState(false)
      const [showUserLogin,setShowUserLogin]=useState(false)
 const value={navigate,user,setUser,isSeller,setisSeller}
