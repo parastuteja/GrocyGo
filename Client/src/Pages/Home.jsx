191825ca9b9ea9ca6957b2eac11549baf0a0f0f0
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-10-15 12:02:34 +0530

    Update Categories

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import MainBanner from '../Components/MainBanner'
+import Categories from '../Components/Categories'
 function Home() {
   return (
-    <div className='mt-10'>
+    <div className='mt-10 ml-10 mr-10'>
 <MainBanner/>
+<Categories/>
     </div>
   )
 }
