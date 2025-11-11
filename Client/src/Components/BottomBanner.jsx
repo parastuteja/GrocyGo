c238dd27322ae9686ba503c067610af7db7feb43
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-11-11 14:21:14 +0530

    Update Bottom Banner

---
+++ Client/src/Components/BottomBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { assets, features } from '../assets/grocygoassets/assets'
+
+function BottomBanner() {
+  return (
+    <div className='relative mt-24'>
+
+<img src={assets.bottom_banner_image} alt="bannner" className='w-full hidden md:block'/>
+<img src={assets.bottom_banner_image_sm} alt="bannner" className='w-full  md:hidden'/>
+<div className='absolute inset-0 flex flex col items-center md:items-end md:justify-center 
+pt-16 md:pt-0 md:pr-24'>
+    <div className='text-2xl md:text-3xl font-semibold text-primary mb-6'>
+        <h1>Why We Are The Best?</h1>
+        {features.map((feature,index)=>(<div key={index} className='flex items-center
+        gap-4 mt-2'>
+<img src={feature.icon} alt={feature.title} className='md:w-11 w-9' />
+<h3>{feature.title}</h3>
+        </div>))}
+        </div>
+</div>
+    </div>
+  )
+}
+
+export default BottomBanner
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MainBanner from '../Components/MainBanner'
 import Categories from '../Components/Categories'
 import BestSeller from '../Components/BestSeller'
+import BottomBanner from '../Components/BottomBanner'
 function Home() {
   return (
     <div className='mt-10 ml-10 mr-10'>
 <MainBanner/>
 <Categories/>
 <BestSeller/>
+<BottomBanner/>
     </div>
   )
 }
