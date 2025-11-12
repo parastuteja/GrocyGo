4469426d6a4e34eaa35b05181e240be4f1128321
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-11-12 22:33:16 +0530

    Update Bottom Banner

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import MainBanner from '../Components/MainBanner'
 import Categories from '../Components/Categories'
 import BestSeller from '../Components/BestSeller'
 import BottomBanner from '../Components/BottomBanner'
+import NewsLetter from '../Components/newsLetter'
+
 function Home() {
   return (
     <div className='mt-10 ml-10 mr-10'>
@@ -10,6 +12,7 @@ function Home() {
 <Categories/>
 <BestSeller/>
 <BottomBanner/>
+<NewsLetter/>
     </div>
   )
 }
