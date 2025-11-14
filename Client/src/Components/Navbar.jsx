edc18924010ae67c514978fe90105b54099967ca
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-11-14 21:56:52 +0530

    update searchbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Navigate, NavLink } from 'react-router-dom'
 import { assets } from '../assets/grocygoassets/assets'
 import { useAppContext } from '../Context/AppContext'
 
 function Navbar() {
 
    const [open, setOpen] = useState(false)
-   const {user,setUser,ShowUserLogin,setShowUserLogin,navigate}=useAppContext()
+   const {user,setUser,ShowUserLogin,setShowUserLogin,navigate,setSearchQuery,searchQuery}=useAppContext()
    const logOut  =()=>{
     setUser(null)
     navigate('/')
    }
+   useEffect(()=>{
+if(searchQuery.length>0){
+   navigate('/products')
+}
+   },[searchQuery])
+
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
@@ -25,7 +31,8 @@ function Navbar() {
                  <NavLink to='/contacts'>Contacts</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e)=> setSearchQuery(e.target.value) }
+                     className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                    <img src={assets.search_icon} className='w-4 h-4'alt='search'/>
                 </div>
 
