c199e18813202b936572507f38e84470d3edc89c
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-11-28 13:18:09 +0530

    Update Orders.jsx

--- Client/src/Pages/Seller/Orders.jsx
+++ Client/src/Pages/Seller/Orders.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../Context/AppContext';
+import { assets, dummyAddress, dummyOrders } from '../../assets/grocygoassets/assets';
 
 function Orders() {
   const {currency}=useAppContext()
-  const [orders,setOrders]
+  const [orders,setOrders]=useState([])
+  const fetchOrders=async()=>{
+setOrders(dummyOrders);
+  }
+  useEffect(()=>{
+fetchOrders();
+  },[])
    return (
+    <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
         <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
                 <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
                     <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={boxIcon} alt="boxIcon" />
+                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
                                 <div key={index} className="flex flex-col justify-center">
                                     <p className="font-medium">
-                                        {item.product.name} <span className={`text-indigo-500 ${item.quantity < 2 && "hidden"}`}>x {item.quantity}</span>
+                                        {item.product.name} <span className='text-primary'>x{item.quantity}</span>
                                     </p>
                                 </div>
                             ))}
                         </>
                     </div>
 
-                    <div className="text-sm">
-                        <p className='font-medium mb-1'>{order.address.firstName} {order.address.lastName}</p>
-                        <p>{order.address.street}, {order.address.city}, {order.address.state},{order.address.zipcode}, {order.address.country}</p>
+                    <div className="text-sm md:text-base text-black/60">
+                        <p className='text-black/80'>{order.address.firstName} {order.address.lastName}</p>
+
+                        <p>{order.address.street}, {order.address.city}, {order.address.state}</p><p>{order.address.zipcode}, {order.address.country}</p>
+                        <p></p>
+                        <p>
+                          {order.address.phone}
+                        </p>
                     </div>
 
-                    <p className="font-medium text-base my-auto text-black/70">${order.amount}</p>
+                    <p className="font-medium text-lg my-auto ">{currency}{order.amount}</p>
 
                     <div className="flex flex-col text-sm">
                         <p>Method: {order.paymentType}</p>
@@ -36,6 +49,7 @@ function Orders() {
                     </div>
                 </div>
             ))}
+        </div>
         </div>
     );
 }
