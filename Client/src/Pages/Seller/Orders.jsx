ea2a87c88a23ee4ba01ad616b579702aea101006
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2025-11-28 13:27:29 +0530

    Update Orders.jsx

--- Client/src/Pages/Seller/Orders.jsx
+++ Client/src/Pages/Seller/Orders.jsx
@@ -16,12 +16,12 @@ fetchOrders();
         <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
-                <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
-                    <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
+                <div key={index} className="flex flex-col  md:items-center  md:flex-row gap-5  justify-between p-5 max-w-4xl rounded-md border border-gray-300">
+                    <div className="flex gap-5 max-w-80">
+                        <img className="w-12 h-12 object-cover " src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
-                                <div key={index} className="flex flex-col justify-center">
+                                <div key={index} className="flex flex-col">
                                     <p className="font-medium">
                                         {item.product.name} <span className='text-primary'>x{item.quantity}</span>
                                     </p>
@@ -42,10 +42,10 @@ fetchOrders();
 
                     <p className="font-medium text-lg my-auto ">{currency}{order.amount}</p>
 
-                    <div className="flex flex-col text-sm">
+                    <div className="flex flex-col text-sm .d:text-base text-black/60">
                         <p>Method: {order.paymentType}</p>
-                        <p>Date: {order.orderDate}</p>
-                        <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+                        <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+                        <p>Payment: {order.isPaid ===true ? "Paid" : "Pending"}</p>
                     </div>
                 </div>
             ))}
