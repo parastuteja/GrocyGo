e221ac7ef3ea07e5fb3b66102810f4db8fbe60f6
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2026-03-23 15:21:12 +0530

    Update Seller

--- Server/controller/productController.js
+++ Server/controller/productController.js
@@ -1,12 +1,48 @@
-export const addProduct=async(req,res)=>{
+import product from "../models/Product"
+import {v2 as cloudinary} from cloudinary
+import product from "../models/Product"
 
+export const addProduct=async(req,res)=>{
+try {
+    let productData=JSON.parse(req.body.productData)
+    const images= req.files
+
+    let imagesUrl=await Promise.all(
+    images.map(async(item)=>{
+let result =await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+return result.secure_url
+    })
+)
+
+await product.create({...productData,image: imagesUrl})
+res.json({success:true})
+} catch (error) {
+    res.json({success:false,message:error.message})
+}
 }
 export const productList=async(req,res)=>{
-
+try {
+    const products =await product.find({})
+    res.json({success:true},products)
+} catch (error) {
+    res.json({success:false,message:error.message})
+}
 }
 export const productById=async(req,res)=>{
-
+try {
+    const {id}=req.body
+    const product =await product.findById(id)
+    res.json({success:true,product})
+} catch (error) {
+    res.json({success:false,message:error.message})
+}
 }
 export const changeStock=async(req,res)=>{
-
+try {
+      const{id,inStock} =req.body
+      await product.findByIdAndUpdate(id,{inStock})
+      res.json({success:true,product})
+} catch (error) {
+    res.json({success:false,message:error.message})
+}
 }
--- Server/server.js
+++ Server/server.js
@@ -5,12 +5,13 @@ import connectDB from './configs/db.js';
 import 'dotenv/config'
 import userRouter from './routes/userRoutes.js';
 import sellerRouter from './routes/sellerRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app= express();
 const AllowedOrigins=['http://localhost:5173']
 const port= process.env.PORT || 4000
 await connectDB()
-
+await connectCloudinary()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:AllowedOrigins, credentials:true}))
